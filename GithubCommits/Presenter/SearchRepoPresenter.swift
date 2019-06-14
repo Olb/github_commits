10e5aa6980a66c9256950f3192e470460c761467
Olb <braybilly@gmail.com>
2019-06-14 15:17:46 +0300

    Add search presenter

---
+++ GithubCommits/Presenter/SearchRepoPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  SearchRepoPresenter.swift
+//  GithubCommits
+//
+//  Created by Billy Bray on 6/14/19.
+//  Copyright © 2019 Billy Bray. All rights reserved.
+//
+
+import Foundation
---
+++ GithubCommitsTests/SearchRepoTests.swift
@@ -0,0 +1,33 @@
+//
+//  SearchRepoTests.swift
+//  GithubCommitsTests
+//
+//  Created by Billy Bray on 6/14/19.
+//  Copyright © 2019 Billy Bray. All rights reserved.
+//
+
+import XCTest
+
+class SearchRepoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
