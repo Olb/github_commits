c2597f7e6ca33b0ccabb555d44a01821b0aeda87
Olb <braybilly@gmail.com>
2019-06-14 16:02:22 +0300

    Completed SearchRepo UI and input validation

--- GithubCommits/View/SearchRepoViewController.swift
+++ GithubCommits/View/SearchRepoViewController.swift
@@ -8,23 +8,79 @@
 
 import UIKit
 
-class SearchRepoViewController: UIViewController {
+class SearchRepoViewController: UIViewController, UITextFieldDelegate, RepoSearchPresenterDelegate {
+    
+    @IBOutlet weak var repoOwnerNameTextField: UITextField!
+    @IBOutlet weak var repoNameTextField: UITextField!
+    @IBOutlet weak var instructionsLabel: UILabel!
+    
+    fileprivate var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    var presenter: SearchRepoPresenter!
 
+    // MARK: - Lifecyle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.presenter = SearchRepoPresenter(delegate: self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBActions
+    
+    @IBAction func searchRepoPressed(_ sender: Any) {
+        self.presenter.search(ownerName: repoOwnerNameTextField.text! , repoName: repoNameTextField.text!)
     }
-    */
+    
+    // MARK: - RepoSearchPresenterDelegate methods
+    
+    func showProgressIndicator() {
+        addActivityIndicator()
+    }
+    
+    func hideProgressIndicator() {
+        removeActivityIndicator()
+    }
+    
+    func repoSearchSuccess(commits: Commits) {
+        // MARK: - TODO - Add success
+    }
+    
+    func reportSearchFailed(message: String) {
+        DispatchQueue.main.async { [weak self] in
+            let alert = UIAlertController(title: "Oops!", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self?.present(alert, animated: true, completion: nil)
+        }
+    }
+}
 
+extension SearchRepoViewController {
+    // MARK: - Activity indicator methods
+    
+    func addActivityIndicator() {
+        self.activityIndicator.hidesWhenStopped = true
+        self.view.addSubview(self.activityIndicator)
+        self.activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        self.activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.activityIndicator.centerYAnchor.constraint(equalTo: self.instructionsLabel.centerYAnchor, constant: -(self.instructionsLabel.frame.size.height + 20)).isActive = true
+        self.activityIndicator.startAnimating()
+    }
+    
+    func removeActivityIndicator() {
+        DispatchQueue.main.async { [weak self] in
+            self?.activityIndicator.stopAnimating()
+            self?.activityIndicator.removeFromSuperview()
+        }
+    }
+    
+    // MARK: - UITextfield delegate methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField == repoOwnerNameTextField) {
+            textField.resignFirstResponder()
+            repoNameTextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
 }
--- GithubCommitsUITests/GithubCommitsUITests.swift
+++ GithubCommitsUITests/GithubCommitsUITests.swift
@@ -9,26 +9,31 @@
 import XCTest
 
 class GithubCommitsUITests: XCTestCase {
-
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        super.setUp()
+        
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testUserSeesErrorMessageWithoutRequiredInfo() {
+        let app = XCUIApplication()
+        app.buttons["Show Commits!"].tap()
+        
+        let titleQuery = app.staticTexts["Oops!"]
+        XCTAssertTrue(titleQuery.exists, "Alert should show with title \"Ooops!\"")
+//
+        let messageQuery = app.staticTexts["Owner name is required."]
+        XCTAssertTrue(messageQuery.exists, "Message should say \"Owner name is required.\" when no owner name entered")
     }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testRepoSearchDismissesOnSuccesfulRepoSearch() {
+        XCTFail()
+    }
+    
+    func testUserSeesCommitsListOnSuccessfulRepoSearch() {
+        XCTFail()
     }
 
 }
