bfb4e7279446465c2f902a3f2cb6547e7f8297a6
Olb <braybilly@gmail.com>
2019-06-14 15:32:47 +0300

    Added RepoSearch controller and view elements

---
+++ GithubCommits/View/SearchRepoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchRepoViewController.swift
+//  GithubCommits
+//
+//  Created by Billy Bray on 6/14/19.
+//  Copyright © 2019 Billy Bray. All rights reserved.
+//
+
+import UIKit
+
+class SearchRepoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
